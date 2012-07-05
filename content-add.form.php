a833e9bb05d274f80bf804ca8c1240b44e48bb5f
skotperez <voraginebramante@gmail.com>
2012-07-05 19:29:29 +0200

    mandatory fields and error messages in add content form

--- content-add.form.php
+++ content-add.form.php
@@ -1,18 +1,25 @@
 <?php
-	$add_form = "
+// errors code construction
+if ( $form_errors[0] != '' ) {
+	$form_error_code = "";
+	foreach ( $form_errors as $form_error ) {
+		$form_error_code .= "<li>" .$form_error. "</li>";
+	}
+}
+	$add_form = "<ul>" .$form_error_code. "</ul>
 	<form id='addcontent' name='addcontent' method='post' action='" .$general_options['blogurl']. "/" .$action_slug. "' enctype='multipart/form-data'>
-		<fieldset>
-			<input id='addcontent-tit' name ='addcontent-tit' type='text' value='' />
+		<fieldset class='required" .$form_tit_class. "'>
+			<input id='addcontent-tit' name ='addcontent-tit' type='text' value='" .$form_tit. "' />
 			<label>Project name</label>
 		</fieldset>
-		<fieldset>
-			<textarea id='addcontent-desc' name='addcontent-desc' cols='45' rows='10'></textarea>
+		<fieldset class='required" .$form_desc_class. "'>
+			<textarea id='addcontent-desc' name='addcontent-desc' cols='45' rows='10'>" .$form_desc. "</textarea>
 			<label>Description</label>
 		</fieldset>
 		<fieldset id='addcontent1' class='clonedField'>
-	    		<input type='hidden' name='MAX_FILE_SIZE' value='5000000' />
-			<input id='addcontent-file' name='addcontent-file' type='file' />
 			<label>Image</label>
+	    		<input type='hidden' name='MAX_FILE_SIZE' value='2000000' />
+			<input id='addcontent-file1' name='addcontent-file1' type='file' />
 		</fieldset>
 		<fieldset class='moreless'>
 			<input class='midbut' type='button' id='addcontent-btnAdd' value='+' />
@@ -20,8 +27,8 @@
 			<label>Want to upload more images?</label>
 		</fieldset>
 		<fieldset>
-			<input id='addcontent-video' name='addcontent-video' type='text' value='' />
-			<select id='addcontent-videoapi' name='addcontent-videoapi' value=''>
+			<input id='addcontent-video' name='addcontent-video' type='text' value='" .$form_video. "' />
+			<select id='addcontent-videoapi' name='addcontent-videoapi' value='" .$form_videoapi. "'>
 				<option value=''></option>
 				<option value='youtube'>Youtube</option>
 				<option value='vimeo'>Vimeo</option>
@@ -33,5 +40,4 @@
 		</fieldset>
 	</form><!-- #addcontent -->
 	";
-
 ?>
