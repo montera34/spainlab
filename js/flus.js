33c5a1a7d3793ec35e3e655d826457a07f2a8b42
numeroteca <pablo@basurama.org>
2012-07-16 13:46:48 +0200

    adding js flus

---
+++ js/flus.js
@@ -0,0 +1,43 @@
+// minimum jQuery gallery
+// click on thumbs nav system
+var $j = jQuery.noConflict();
+$j(document).ready(function(){
+	$j('.zoom-item').hide();
+	$j('.zoom-item:first').show().addClass('visto');
+	$j('.single-thumb:first').addClass('access-off');
+	// thumbs classes
+	$j('div.single-thumb img').each(
+		function( j ) {
+			var $counter = (j+1);
+			var $access = 'access-' + $counter;
+			$j(this).addClass($access);
+		}
+	);
+	// zoom items classes and click effect
+	var $maxHeight = 0;
+	$j('div.zoom-item').each(function( i ) {
+		var $currentHeight = $j(this).height();
+		if ( $currentHeight > $maxHeight ) {
+			$maxHeight = $currentHeight;
+		}
+			var $counter = (i+1);
+			var $access = 'access-' + $counter;
+			$j(this).addClass($access);
+			$j('img.'+ $access ).click(function() {
+				if ( $j(this).hasClass('access-off') ) {
+				} else {
+					$j('.access-off').removeClass('access-off');
+					$j('.visto').fadeOut('100').removeClass('visto');
+					$j(this).addClass('access-off');
+					$j('div.' + $access ).fadeIn('800').addClass('visto');
+				}
+			});
+	});
+	$maxHeight = $maxHeight+20;
+	$j('#visor').css({height: $maxHeight + 'px', position: 'absolute', top: '0px'});
+	$maxHeight = $maxHeight+80;
+	$j('#single-gallery').css({height: $maxHeight + 'px'});
+	$j('.zoom-item').css({position: 'absolute', top: '0px'});
+	$j('.part-single-gal').css({'min-height': $maxHeight + 'px'});
+	$j('#selector').css({position: 'absolute', bottom: '0px'});
+});
